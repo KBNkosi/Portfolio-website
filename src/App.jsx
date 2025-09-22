cfb9badf8bce65485e9cd954926e2437bb4d848d
Nkosikenny <kennybhekumuzi@gmail.com>
2025-09-22 16:16:11 +0200

    created 404 file for redirects

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { AnimatePresence } from 'framer-motion'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import Footer from './components/Footer'
@@ -8,22 +7,22 @@ import Contact from './pages/Contact'
 
 const App = () => {
   return (
-    <>
+    
     <Router>
       <Navbar/>
-      <AnimatePresence mode='wait'>
+     
        <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='projects' element={<Projects />}/>
         <Route path='contact' element={<Contact />}/>
       </Routes>
-      </AnimatePresence>
+     
       
      
      <Footer/>
     </Router>
     
-    </>
+   
   )
 }
 
